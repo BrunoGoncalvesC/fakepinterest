cf397d8e8dc830caf2dad68e6ef7dafc11a808a6
bruno correa <bgcr9831@gmail.com>
2023-08-29 22:26:35 -0300

    Mudanca de banco db

--- criar_Banco.py
+++ criar_Banco.py
@@ -2,10 +2,7 @@ from fakepinterest import database,app
 from fakepinterest.models import Usuario,Foto
 
 with app.app_context():
-    user = Foto.query.all()
-    for foto in user:
-       print(foto)
-
+    database.create_all()
 
 
 
--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -9,6 +9,7 @@ import os
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://banco_fakepin_user:QfqIZtuTnWGg8NdKA9qYTbTrt6mO8DOi@dpg-cjn9dclhe99c739t1eqg-a.oregon-postgres.render.com/banco_fakepin"
 app.config["SECRET_KEY"] = '9802f117b391f76be04968bd914236dd0b8f921ae78811e8757cd0c302216424'
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
